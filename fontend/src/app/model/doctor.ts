0fd1a90c11f24cf5dd1be129f95ff6adeafc9893
VaradScript <108749445+VaradScript@users.noreply.github.com>
2025-05-13 18:42:55 +0530

    backend done
    all the spring boot files are there

--- fontend/src/app/model/doctor.ts
+++
@@ -1,7 +0,0 @@
-export class Doctor{
-    id?:number;
-    firstname:string=""
-    lastname:string=""
-    username:string=""
-    password:string=""
-}
--- fontend/src/app/patient.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Patient } from './model/patient';
-
-describe('Patient', () => {
-  it('should create an instance', () => {
-    expect(new Patient()).toBeTruthy();
-  });
-});
